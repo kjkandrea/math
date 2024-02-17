1110c0c14db4e4d78845028d1ed7edd997795a41
kjkandrea <kjkandrea@gmail.com>
2024-02-17 13:55:52 +0900

    test: jemini 리뷰 반영. 오류처리 테스트

--- junior/1-year/05_volumeCalculator.spec.js
+++ junior/1-year/05_volumeCalculator.spec.js
@@ -2,17 +2,35 @@ import { describe, expect, it } from "@jest/globals";
 import { calcVolume } from "./05_volumeCalculator";
 
 describe("calcVolume", () => {
-  it("calcCylinderVolume", () => {
-    expect(calcVolume.calcCylinderVolume(3, 5)).toBe(3 ** 2 * Math.PI * 5);
+  describe("calcCylinderVolume", () => {
+    it("calculates correctly for positive radius and height", () => {
+      expect(calcVolume.calcCylinderVolume(3, 5)).toBe(3 ** 2 * Math.PI * 5);
+    });
+
+    it("calculates incorrectly for less then 0 radius or height", () => {
+      expect(() => calcVolume.calcCylinderVolume(0, -5)).toThrow();
+    });
   });
 
-  it("calcConeVolume", () => {
-    expect(calcVolume.calcConeVolume(3, 5)).toBe(
-      3 ** 2 * Math.PI * 5 * (1 / 3),
-    );
+  describe("calcConeVolume", () => {
+    it("calculates correctly for positive radius and height", () => {
+      expect(calcVolume.calcConeVolume(3, 5)).toBe(
+        3 ** 2 * Math.PI * 5 * (1 / 3),
+      );
+    });
+
+    it("calculates incorrectly for less then 0 radius or height", () => {
+      expect(() => calcVolume.calcConeVolume(0, -5)).toThrow();
+    });
   });
 
-  it("calcSphereVolume", () => {
-    expect(calcVolume.calcSphereVolume(3)).toBe(3 ** 3 * Math.PI * (4 / 3));
+  describe("calcSphereVolume", () => {
+    it("calculates correctly for positive radius", () => {
+      expect(calcVolume.calcSphereVolume(3)).toBe(3 ** 3 * Math.PI * (4 / 3));
+    });
+
+    it("calculates incorrectly for less then 0 radius", () => {
+      expect(() => calcVolume.calcSphereVolume(0)).toThrow();
+    });
   });
 });

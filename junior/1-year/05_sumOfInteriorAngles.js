469ea839a1972f002c071192a4dddcf55628c429
kjkandrea <kjkandrea@gmail.com>
2024-02-17 13:59:12 +0900

    refactor: jemini 리뷰 반영. 코드 스타일 / JSDoc

--- junior/1-year/05_sumOfInteriorAngles.js
+++ junior/1-year/05_sumOfInteriorAngles.js
@@ -1,7 +1,14 @@
-export function sumOfInteriorAngles(vertices) {
-  if (2 >= vertices) {
-    throw new Error("The vertices must be greater than 2");
+/**
+ * Calculates the sum of the interior angles of a polygon.
+ *
+ * @param {number} polygonSides - The number of vertices in the polygon.
+ * @returns {number} The sum of the interior angles of the polygon in degrees.
+ * @throws {Error} If the number of vertices is less than or equal to 2.
+ */
+export function sumOfInteriorAngles(polygonSides) {
+  if (2 >= polygonSides) {
+    throw new Error("The number of sides must be greater than 2");
   }
 
-  return (vertices - 2) * 180;
+  return (polygonSides - 2) * 180;
 }

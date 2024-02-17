060d0ccd10b2a2378be505fecf81b20e2b91f209
kjkandrea <kjkandrea@gmail.com>
2024-02-17 12:36:25 +0900

    feat: calcVolume

---
+++ junior/1-year/volume.js
@@ -0,0 +1,11 @@
+const calcVolume = {
+  clinder(radius, height) {
+    return radius ** 2 * height * Math.PI
+  },
+  cone(radius, height) {
+    return radius ** 2 * height * Math.PI * (1/3)
+  },
+  sphere(radius) {
+    return radius ** 3 * Math.PI * (4/3)
+  }
+}
